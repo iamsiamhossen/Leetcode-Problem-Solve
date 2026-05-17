631e82e443a12509276cdc6e310bb99842687096
iamsiamhossen <141014441+iamsiamhossen@users.noreply.github.com>
2026-05-17 22:10:48 +0600

    Time: 8 ms (32.91%) | Memory: 11.3 MB (14.20%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
+           
         if(s1.size() > s2.size()) return false;
-
+        
         map<char,int> m, n;
-        for(auto u : s1) m[u]++;
-        int k = s1.size(); 
+        for(auto u : s1){
+            m[u]++;
+        }
+        
+        int l = 0, tmp = 0;
         for(int r = 0; r < s2.size(); r++){
             n[s2[r]]++;
-            if(r >= k){
-               
-                char leftChar = s2[r - k];
-                n[leftChar]--;
-                if(n[leftChar] == 0) n.erase(leftChar);
-            }
-            if(r >= k - 1){
-                if(m == n) return true;
+            tmp++;
+            
+            if(tmp == s1.size()){
+                if(m == n){ return true; }
+                
+                n[s2[l]]--;
+                if(n[s2[l]] == 0) n.erase(s2[l]); 
+                l++;
+                tmp--;
             }
         }
-
         return false;
     }
 };
