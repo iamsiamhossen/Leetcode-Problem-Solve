2877b4db32e952b5210bff474b81347e6265e38e
iamsiamhossen <141014441+iamsiamhossen@users.noreply.github.com>
2026-05-19 04:01:36 +0600

    Time: 0 ms (100.00%) | Memory: 11.5 MB (17.06%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,37 +1,26 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        
-        // Step 1: পুরো string reverse
-        reverse(s.begin(), s.end());
-
-        // Step 2: প্রতিটা word reverse
-        int start = 0;
-        for(int i = 0; i <= s.size(); i++){
-            if(i == s.size() || s[i] == ' '){
-                reverse(s.begin() + start, s.begin() + i);
-                start = i + 1;
-            }
+            stringstream ss(s);
+    vector<string> arr;
+    string temp;
+    string word;
+    while (ss >> word)
+    {
+        arr.push_back(word);
+    }
+    reverse(arr.begin(), arr.end());
+    int x1=0;
+    for(auto &x : arr)    {
+        x1++;
+        if(x1==arr.size()){
+            temp += x;
         }
-
-        // Step 3: Extra spaces remove
-        int i = 0, j = 0;
-        while(j < s.size()){
-            // space skip koro
-            while(j < s.size() && s[j] == ' ') j++;
-            
-            // word copy koro
-            while(j < s.size() && s[j] != ' '){
-                s[i++] = s[j++];
-            }
-            
-            // word er পরে একটা space দাও
-            if(j < s.size()) s[i++] = ' ';
+        else{
+            temp += x + " ";
         }
-
-        // trailing space remove
-        if(i > 0 && s[i-1] == ' ') i--;
         
-        return s.substr(0, i);
+    }
+    return temp;
     }
 };
