d5472d34446800ce62d8b97682c7153acbce8f78
iamsiamhossen <141014441+iamsiamhossen@users.noreply.github.com>
2026-05-27 14:34:54 +0600

    Time: 31 ms (95.62%) | Memory: 45.5 MB (38.50%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> mp;
+        // PrefixSum+Hashing Solve For Negative Value
+        unordered_map<int,int> mp;
+        mp[0]=1;
+        int prefixSum=0;
+        int cnt =0;
+        for(int i=0;i<nums.size();i++){
+            prefixSum+=nums[i];
+            int need = prefixSum-k;
+            if(mp.count(need)){
+                cnt+= mp[need];
+            }
+            mp[prefixSum]++;
 
-    mp[0] = 1;  
-
-    int prefixSum = 0;
-    int cnt = 0;
-
-    for (int i = 0; i < nums.size(); i++)
-    {
-        prefixSum += nums[i];
-
-        int need = prefixSum - k;
-
-        if (mp.find(need) != mp.end())
-        {
-           cnt+=mp[need];
         }
-
-        mp[prefixSum]++;
-    }
-    return cnt;
+        return cnt;
     }
 };
